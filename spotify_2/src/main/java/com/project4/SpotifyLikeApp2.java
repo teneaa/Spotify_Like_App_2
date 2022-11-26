d313303600cb005af44ed4e4a007542bcd628a08
Tenea Allen <112679362+teneaa@users.noreply.github.com>
2022-11-26 15:27:28 -0800

    removed year data -- try new implementation

--- spotify_2/src/main/java/com/project4/SpotifyLikeApp2.java
+++ spotify_2/src/main/java/com/project4/SpotifyLikeApp2.java
@@ -18,7 +18,7 @@ public class SpotifyLikeApp2 {
     static HashMap<String,String> titleSearch = new HashMap<>();
     static HashMap<String,String> findArtist = new HashMap<>();
     static HashMap<String,String> getGenre = new HashMap<>();
-    static Map<String,Integer> getSongYear = new HashMap<String,Integer>();
+    //static Map<String,Integer> getSongYear = new HashMap<String,Integer>();
     static Map<String,Integer> playSongFile = new HashMap<String,Integer>();
     static String theSong;
   
@@ -53,7 +53,7 @@ public class SpotifyLikeApp2 {
         String artist = (String) Items.get("artist");
         // Objects that still need to be placed into hashmaps:
         String genre = (String) Items.get("genre");
-        Integer releaseYear = (Integer) Items.get("year");
+        //Integer releaseYear = (Integer) Items.get("year");
 
         Integer songNum = i;
 
@@ -67,7 +67,7 @@ public class SpotifyLikeApp2 {
         getGenre.put((song),(genre));
 
         //Place song and release year into getSongYear HashMap
-        getSongYear.put((song),(releaseYear));
+        //getSongYear.put((song),(releaseYear));
 
         //Place song and index number into playSongFile HashMap
         playSongFile.put((song),(songNum));
@@ -170,7 +170,7 @@ public static void handleCaseL(JSONArray library, Scanner sc) {
       String song_name = (String) songs.get("name");
       System.out.print(i + 1 + "| ");
       System.out.print("Song: " +  song_name + "| Artist: " + findArtist.get(song_name));
-      System.out.print(" | Year: " + getSongYear.get(song_name));
+      //System.out.print(" | Year: " + getSongYear.get(song_name));
       System.out.print(" | Genre: " + getGenre.get(song_name));
       System.out.println("| File: " + titleSearch.get(song_name) + "\n");
     }
